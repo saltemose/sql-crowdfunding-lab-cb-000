d9c41ca634612a600df04eb4afb17d4c707b3948
Sean Altemose <sean@seanaltemose.com>
2018-06-21 21:38:06 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
 
-CREATE TABLE user (id PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE users (id PRIMARY KEY, name TEXT, age INTEGER);
 
-CREATE TABLE pledge (id PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
+CREATE TABLE pledges (id PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
